862d1ed83ed22f4a07d67b1ac239916661407325
jaeunP <okn2432@gmail.com>
2023-05-07 19:43:45 +0900

    jwt

--- src/main/java/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/example/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package example.springsecurity.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,11 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) {
+//        h2 하위의 모든 요청, favicon 요청은 무시
+        web.ignoring().antMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests()    //HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/api/hello").permitAll()  //URL에 대한 요청은 인증없이 접근 허용
+                .anyRequest().authenticated();  //나머지 요청들은 모두 인증 요구
     }
 
 }
