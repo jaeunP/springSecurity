f6b9d58b8b0a78006763d3d7ac449cc479ca1cb3
jaeunP <okn2432@gmail.com>
2023-05-11 16:10:40 +0900

    jwt

---
+++ src/main/java/example/springsecurity/controller/UserController.java
@@ -0,0 +1,41 @@
+package example.springsecurity.controller;
+
+import example.springsecurity.dto.UserDto;
+import example.springsecurity.entity.User;
+import example.springsecurity.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@Valid @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+    
+    /**
+     * @Preauthoirze를 통해서 User, ADMIN 두가지 권한 모두 허용
+     */
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<User> getMyUserInfo() {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+    
+    /** ADMIN 권한만 호출할수 있도록 설정  */
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<User> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+}
---
+++ src/main/java/example/springsecurity/service/UserService.java
@@ -0,0 +1,66 @@
+package example.springsecurity.service;
+
+import example.springsecurity.dto.UserDto;
+import example.springsecurity.entity.Authority;
+import example.springsecurity.entity.User;
+import example.springsecurity.repository.UserRepository;
+import example.springsecurity.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /**
+     * 회원가입 로직을 수행하는 메서드
+     * username이 DB에 존재하지 않으면 Authority와 User 정보를 생성해서
+     * UserRepository의 save를 통해 DB에 정보를 저장
+     */
+    @Transactional
+    public User signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return userRepository.save(user);
+    }
+    /**
+     * username을 기준으로 권한정보를 가져옴
+     */
+    @Transactional(readOnly = true)
+    public Optional<User> getUserWithAuthorities(String username) {
+        return userRepository.findOneWithAuthoritiesByUsername(username);
+    }
+
+
+    /**
+     * SecurityContext에 저장된 username의 권한 정보만 가져옴
+     */
+    @Transactional(readOnly = true)
+    public Optional<User> getMyUserWithAuthorities() {
+        return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
+    }
+}
